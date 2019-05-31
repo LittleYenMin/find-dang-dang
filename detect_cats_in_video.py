21a84117cad9748060210cd18b1fddda830a7f4b
LittleYenMin <fkym08@gmail.com>
2019-05-31 23:02:35 +0800

    Extract script syntax to become the methods.

--- detect_cats_in_video.py
+++ detect_cats_in_video.py
@@ -1,71 +1,52 @@
 import os
 import numpy as np
 import cv2
-import dotenv
 
-dotenv.load_dotenv()
+from config_r_cnn import model
 
 
-# Filter a list of Mask R-CNN detection results to get only the detected cats / dogs
+# Filter a list of Mask R-CNN detection results to get only the detected
+# cats / dogs
 def get_pet_boxes(boxes, class_ids):
     pet_boxes = []
-    for i, box in enumerate(boxes):
-        # If the detected object isn't a car / truck, skip it
-        if class_ids[i] in [16, 17]:
+    for box, class_id in zip(boxes, class_ids):
+        if class_id in [16, 17]:
             pet_boxes.append(box)
     return np.array(pet_boxes)
+
+
+def detect_objects(frame):
+    # Convert the image from BGR color (which OpenCV uses) to RGB color
+    rgb_image = frame[:, :, ::-1]
+    return model.detect([rgb_image], verbose=0)
+
+
+def draw_boxes(boxes, frame):
+    for box in boxes:
+        y1, x1, y2, x2 = box
+        # Draw the box
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+    return frame
 
 
 # Load the video file we want to run detection on
 video_capture = cv2.VideoCapture(os.getenv('SOURCE_VIDEO'))
 
+# Loop over each frame of video
+while video_capture.isOpened():
+    success, frame = video_capture.read()
+    if not success:
+        break
 
-if __name__ == '__main__':
-    from config_r_cnn import model
+    # Pass single image to Mask R-CNN detect pipline.
+    results = detect_objects(frame)
 
-    # Loop over each frame of video
-    while video_capture.isOpened():
-        success, frame = video_capture.read()
-        if not success:
-            break
+    # Get first result cause we only pass single image to detection pipline.
+    r = results[0]
+    pet_boxes = get_pet_boxes(r['rois'], r['class_ids'])
+    # Draw each box on the frame and show.
+    cv2.imshow('Video', draw_boxes(boxes=pet_boxes, frame=frame))
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color
-        rgb_image = frame[:, :, ::-1]
-
-        # Run the image through the Mask R-CNN model to get results.
-        results = model.detect([rgb_image], verbose=0)
-
-        # Mask R-CNN assumes we are running detection on multiple images.
-        # We only passed in one image to detect, so only grab the first result.
-        r = results[0]
-
-        # The r variable will now have the results of detection:
-        # - r['rois'] are the bounding box of each detected object
-        # - r['class_ids'] are the class id (type) of each detected object
-        # - r['scores'] are the confidence scores for each detection
-        # - r['masks'] are the object masks for each detected object (which gives you the object outline)
-
-        # Filter the results to only grab the car / truck bounding boxes
-        pet_boxes = get_pet_boxes(r['rois'], r['class_ids'])
-
-        print("Pets found in frame of video:")
-
-        # Draw each box on the frame
-        for box in pet_boxes:
-            print("pet: ", box)
-
-            y1, x1, y2, x2 = box
-
-            # Draw the box
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
-
-        # Show the frame of video on the screen
-        cv2.imshow('Video', frame)
-
-        # Hit 'q' to quit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Clean up everything when finished
-    video_capture.release()
-    cv2.destroyAllWindows()
+# Clean up everything when finished
+video_capture.release()
+cv2.destroyAllWindows()
